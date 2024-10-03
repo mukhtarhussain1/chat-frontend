27cb2ac677781aec7790a004e0ba8b6f1be44d11
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-10-04 00:56:34 +0500

    add: server file

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('join', (userId) => {
+    socket.join(userId);
+    console.log(`User ${userId} joined`);
+  });
+
+  socket.on('sendMessage', (data) => {
+    const { senderId, receiverId, message } = data;
+    io.to(receiverId).emit('receiveMessage', { senderId, message });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
