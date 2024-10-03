7a55bc038044103294fe09241f845596ac622103
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-10-04 00:57:45 +0500

    add: required icons

---
+++ src/assets/index.jsx
@@ -0,0 +1,4 @@
+export { default as attachIcon } from "./icons/attach.svg";
+// export { default as ProfileImage } from "./images/profile.png";
+export { default as recordIcon } from "./icons/record.svg";
+export { default as sendMessageIcon } from "./icons/send-message.svg";
